a21d7de88504c4b690d4fbdafdeea71c41f03489
evgeniy volkov <volkov.e.n@gmail.com>
2021-03-11 13:19:15 +0300

    init commit

---
+++ src/test/java/ru/volkov/batch/skip/SkipApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.volkov.batch.skip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
